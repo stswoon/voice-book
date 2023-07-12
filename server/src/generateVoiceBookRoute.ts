67aa1999a3ab5a421ce5b772f42cff7f45b63d9d
nekhozhin <nekhozhin@netcracker.com>
2023-07-12 10:25:03 +0400

    multiple not works

--- server/src/generateVoiceBookRoute.ts
+++ server/src/generateVoiceBookRoute.ts
@@ -1,24 +1,36 @@
 import {Router} from "express";
 import fse from "fs-extra";
-import {spawn} from "child_process";
+// import {spawn} from "child_process";
 import ffmpeg from "fluent-ffmpeg";
 import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
 import {uniqueNamesGenerator, adjectives, colors, animals} from "unique-names-generator";
 import {Task, TaskPool} from '@antmind/task-pool';
 import {glob} from "glob";
+import {translitToRussian} from "./textTranslits";
+import {Stream, Readable, Writable} from "stream";
 
 const generateName = (): string => uniqueNamesGenerator({dictionaries: [adjectives, colors, animals]}); // big_red_donkey
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 export const generateVoiceBookRoutes = Router();
 
-const progress: any = {};  //id, {status, date}
+type ProgressType = {
+    [key: string]: {
+        status: string
+        startDate: number
+        fileBuffers: {
+            [key: string]: Buffer
+        }
+        outputStream?: Stream;
+    }
+}
+const progress: ProgressType = {};  //id, {status, date}
 
 generateVoiceBookRoutes.post("/", async (req, res) => {
     const id = generateName();
     try {
         const text = req.body.text;
-        progress[id] = {status: 'queue 1/1', startDate: (new Date()).getTime()}; //todo queue progress
+        progress[id] = {status: 'queue 1/1', startDate: (new Date()).getTime(), fileBuffers: {}}; //todo queue progress
         runVoiceBook(id, text);
         return res.status(200).json({processId: id, status: "queue"});
     } catch (error) {
@@ -30,7 +42,15 @@ generateVoiceBookRoutes.post("/", async (req, res) => {
 
 generateVoiceBookRoutes.get("/:id", async (req, res) => {
     const id = req.params.id;
-    return res.download(`${bookRunsPath}/${id}/concatenated-audio.mp3`)
+    res.attachment('audio.mp3');
+    if (progress[id].outputStream) {
+        progress[id].outputStream!.pipe(res);
+    } else {
+        res.status(404);
+    }
+    // pdfstream.pipe(res);
+    // return res.download(progress[id].outputStream)
+    // return res.download(`${bookRunsPath}/${id}/concatenated-audio.mp3`)
 });
 
 generateVoiceBookRoutes.get("/progress/:id", async (req, res) => {
@@ -56,7 +76,8 @@ const SILERO_MAX_LEN = 900; //sometimes fails if near 1000
 
 async function runVoiceBook(id: string, text: string): Promise<void> {
     text = translitToRussian(text);
-    const textItems = splitText(text, SILERO_MAX_LEN);
+    // const textItems = splitText(text, SILERO_MAX_LEN);
+    const textItems = splitTextSimple(text);
     if (textItems.length === 0) {
         progress[id].status = "error";
         console.error("empty text");
@@ -68,11 +89,20 @@ async function runVoiceBook(id: string, text: string): Promise<void> {
 }
 
 //TODO: maybe split by sentence is better to sounds
+function splitTextSimple(text: string): string[] {
+    text = text.trim();
+    let items = text.split(/[.?!]+/);
+    items = items.filter(item => {
+        return item.trim().length !== 0;
+    });
+    return items;
+}
 
 //export only for test
 export function splitText(text: string, maxLen: number = 1000): string[] {
     text = text.trim();
-    const replacer = (match: string, p1: string, offset: number, string: string): string => {
+    // const replacer = (match: string, p1: string, offset: number, string: string): string => {
+    const replacer = (match: string, p1: string): string => {
         return p1 + "${splitter}";
     };
     text = text.replace(/([.?!]+)/g, replacer);
@@ -128,7 +158,7 @@ async function generateAudios(textItems: string[], id: string): Promise<void> {
     }
     await fse.mkdir(`${bookRunsPath}/${id}`);
 
-    progress[id].status = 0;
+    progress[id].status = String(0);
     const progressDelta = 95 / textItems.length;
 
     const pool = new TaskPool({concurrency: POOL_LIMIT});
@@ -136,7 +166,8 @@ async function generateAudios(textItems: string[], id: string): Promise<void> {
         const task = new Task(async (i: any) => {
             console.log(`run task ${i}`);
             await generateAudio(textItems[i], `${bookRunsPath}/${id}/${i}`);
-            progress[id].status = Math.round(progress[id].status + progressDelta);
+            progress[id].fileBuffers[i] = await fse.readFile(`${bookRunsPath}/${id}/${i}/test.wav`);
+            progress[id].status = String(Math.round(Number(progress[id].status + progressDelta)));
             console.log(`finish task ${i}`);
         }, i);
         pool.addTask(task);
@@ -192,180 +223,59 @@ async function convertToMp3(wavFilename: string) {
 
 
 async function glueFiles(id: string, length: number) {
-    console.log("wait 10 sec");
-    await new Promise(resolve => setTimeout(() => resolve(), 10 * 1000));
+    // console.log("wait 10 sec");
+    // await new Promise(resolve => setTimeout(() => resolve(), 10 * 1000));
     console.log("converting files...");
 
-    const files = await glob(`${bookRunsPath}/${id}` + '/**/test.wav', {ignore: 'node_modules/**'})
-    console.log(files);
+    // const files = await glob(`${bookRunsPath}/${id}` + '/**/test.wav', {ignore: 'node_modules/**'})
+    console.log("(progress[id].fileBuffers = ", progress[id].fileBuffers);
 
-    const convertPromises = []
+    const ffmpegProcess = ffmpeg();
+    const { StreamInput, StreamOutput } = require('fluent-ffmpeg-multistream')
     for (let i = 0; i < length; ++i) {
-        let filepath = `${bookRunsPath}/${id}/${i}/test.wav`
-        console.log("File path: " + filepath);
-        const path = require("path");
-        filepath = path.normalize!(filepath)!;
-        console.log("Simple file path: " + filepath);
-        convertPromises.push(convertToMp3(filepath))
+        ffmpegProcess.addInput(StreamInput(Readable.from(progress[id].fileBuffers[i])).url);
     }
-    await Promise.all(convertPromises);
+    const outputStream = new Writable();
+    await new Promise(((resolve, reject) => {
+        ffmpegProcess.output(outputStream)
+            .on("error", (err) => reject(err))
+            .on("end", () => resolve("ok"))
+            .run();
+    }));
+    progress[id].outputStream = outputStream;
 
-    //https://www.reddit.com/r/node/comments/qoya5y/how_to_convert_audio_from_wav_to_mp3_in_nodejs/
-    //https://5k-team.trilogy.com/hc/en-us/articles/360016761260-How-to-Concatenate-Audio-Files-in-NodeJS
-    //https://www.reddit.com/r/node/comments/qoya5y/comment/hjrsepb/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-
-    // const { exec } = require('child_process')
-    // exec(`ffmpeg -i ${wavFilePath} ${otherffmpegOpts} ${mp3FilePath}`, (err, stdout, stderr) => {
-    //     // callback here for errors etc
-    // })
-
-    console.log("concatinating files...")
-    const audioconcat = require('audioconcat');
-    const songs = []
-    for (let i = 0; i < length; ++i) {
-        songs.push(`${bookRunsPath}/${id}/${i}/test.mp3`);
-    }
-    //https://www.npmjs.com/package/audioconcat
-    audioconcat(songs)
-        .concat(`${bookRunsPath}/${id}/concatenated-audio.mp3`)
-        .on('error', (error: any) => console.error('Failed to concatenate files', error))
-        .on('end', () => console.info('Audio prompts generated'));
+    //
+    // const convertPromises = []
+    // for (let i = 0; i < length; ++i) {
+    //     //let filepath = `${bookRunsPath}/${id}/${i}/test.wav`
+    //     //console.log("File path: " + filepath);
+    //     //const path = require("path");
+    //     //filepath = path.normalize!(filepath)!;
+    //     //console.log("Simple file path: " + filepath);
+    //     convertPromises.push(convertToMp3(id, i))
+    // }
+    // await Promise.all(convertPromises);
+    //
+    // //https://www.reddit.com/r/node/comments/qoya5y/how_to_convert_audio_from_wav_to_mp3_in_nodejs/
+    // //https://5k-team.trilogy.com/hc/en-us/articles/360016761260-How-to-Concatenate-Audio-Files-in-NodeJS
+    // //https://www.reddit.com/r/node/comments/qoya5y/comment/hjrsepb/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    //
+    // // const { exec } = require('child_process')
+    // // exec(`ffmpeg -i ${wavFilePath} ${otherffmpegOpts} ${mp3FilePath}`, (err, stdout, stderr) => {
+    // //     // callback here for errors etc
+    // // })
+    //
+    // console.log("concatinating files...")
+    // const audioconcat = require('audioconcat');
+    // const songs = []
+    // for (let i = 0; i < length; ++i) {
+    //     songs.push(`${bookRunsPath}/${id}/${i}/test.mp3`);
+    // }
+    // //https://www.npmjs.com/package/audioconcat
+    // audioconcat(songs)
+    //     .concat(`${bookRunsPath}/${id}/concatenated-audio.mp3`)
+    //     .on('error', (error: any) => console.error('Failed to concatenate files', error))
+    //     .on('end', () => console.info('Audio prompts generated'));
 }
 
-function translitToRussian(s: string): string {
-    function safeTranslit(char: string) {
-        switch (char) {
-            case "A":
-                return "А";
-            case "a":
-                return "a";
-            case "B":
-                return "Б";
-            case "b":
-                return "б";
-            case "C":
-                return "Ц";
-            case "c":
-                return "ц";
-            case "D":
-                return "Д";
-            case "d":
-                return "д";
-            case "E":
-                return "И";
-            case "e":
-                return "и";
-            case "F":
-                return "Ф";
-            case "f":
-                return "ф";
-            case "G":
-                return "Г";
-            case "g":
-                return "г";
-            case "H":
-                return "Х";
-            case "h":
-                return "х";
-            case "I":
-                return "И";
-            case "i":
-                return "и";
-            case "J":
-                return "Ж";
-            case "j":
-                return "ж";
-            case "K":
-                return "К";
-            case "k":
-                return "к";
-            case "L":
-                return "Л";
-            case "l":
-                return "л";
-            case "M":
-                return "М";
-            case "m":
-                return "м";
-            case "N":
-                return "Н";
-            case "n":
-                return "н";
-            case "O":
-                return "О";
-            case "o":
-                return "о";
-            case "P":
-                return "П";
-            case "p":
-                return "п";
-            case "Q":
-                return "К";
-            case "q":
-                return "к";
-            case "R":
-                return "Р";
-            case "r":
-                return "р";
-            case "S":
-                return "С";
-            case "s":
-                return "с";
-            case "T":
-                return "Т";
-            case "t":
-                return "т";
-            case "U":
-                return "У";
-            case "u":
-                return "у";
-            case "V":
-                return "В";
-            case "v":
-                return "в";
-            case "W":
-                return "В";
-            case "w":
-                return "в";
-            case "X":
-                return "Экс";
-            case "x":
-                return "экс";
-            case "Y":
-                return "Й";
-            case "y":
-                return "й";
-            case "Z":
-                return "З";
-            case "z":
-                return "з";
-            case "0":
-                return "ноль";
-            case "1":
-                return "один";
-            case "2":
-                return "два";
-            case "3":
-                return "три";
-            case "4":
-                return "четыре";
-            case "5":
-                return "пять";
-            case "6":
-                return "шесть";
-            case "7":
-                return "семь";
-            case "8":
-                return "восемь";
-            case "9":
-                return "девять";
-        }
-        return char;
-    }
 
-    let result = "";
-    for (let i = 0; i < s.length; ++i) {
-        result += safeTranslit(s[i]);
-    }
-    return result
-}
